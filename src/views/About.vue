743b9a108c2cac3179ec28d1ee24e6e07fcf0a4e
pete <ptrmutie@gmail.com>
2020-12-14 14:37:46 +0300

    added todo page and about page and routed the pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About vuetify todo</h1>
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+      molestiae quisquam, explicabo deleniti temporibus accusantium repellat
+      voluptate sit impedit sed magni omnis a natus tempore accusamus,
+      exercitationem, quae praesentium velit?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+      molestiae quisquam, explicabo deleniti temporibus accusantium repellat
+      voluptate sit impedit sed magni omnis a natus tempore accusamus,
+      exercitationem, quae praesentium velit?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+      molestiae quisquam, explicabo deleniti temporibus accusantium repellat
+      voluptate sit impedit sed magni omnis a natus tempore accusamus,
+      exercitationem, quae praesentium velit?
+    </p>
   </div>
 </template>
